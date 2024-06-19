c74f9e5c65f1a89572ea4684efe7d25852303a05
Paddy <paddyfedoo@hotmail.com>
2024-06-19 13:03:38 +0100

    Using Svelte to load an API call with fetch

--- src/components/TestSvelte.svelte
+++ src/components/TestSvelte.svelte
@@ -1,5 +1,24 @@
 <script>
-    let name = 'Svelte'
-    const urlParams = new URLSearchParams(window.location.search);
+  import { onMount } from "svelte";
+
+  let name = "Svelte";
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get("movieId");
+
+  let promise = fetch("https://api.coindesk.com/v1/bpi/currentprice.json").then(
+    (x) => x.json()
+  );
 </script>
+
 <h1>Hello {name} {urlParams}</h1>
+
+{#await promise then data}
+  <pre>
+  {JSON.stringify(data, null, 2)}
+</pre>
+  {data.chartName}
+  {data.bpi.USD.code}
+  {@html data.bpi.USD.symbol}
+{:catch error}
+  {error}
+{/await}
