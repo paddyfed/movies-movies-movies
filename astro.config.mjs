fdaa58f4b2f20359c27b2b51e543e042886ce487
Paddy <paddyfedoo@hotmail.com>
2024-06-19 11:54:04 +0100

    Testing very basic Svelte component in Astro page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://paddyfed.github.io",
   base: "/movies-movies-movies",
+  integrations: [svelte()]
 });
---
+++ src/components/TestSvelte.svelte
@@ -0,0 +1,5 @@
+<script>
+    let name = 'Svelte'
+    const urlParams = new URLSearchParams(window.location.search);
+</script>
+<h1>Hello {name} {urlParams}</h1>
