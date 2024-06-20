bb8bfe690c39f754c0618c2a69e9147f5d12556b
Paddy <paddyfedoo@hotmail.com>
2024-06-20 21:21:48 +0100

    Basic movie scroll pagination

--- src/components/MovieScroll.svelte
+++ src/components/MovieScroll.svelte
@@ -4,9 +4,10 @@
   export let scrollTitle;
   export let fetchUrl;
 
-  const imgPosterUrl = "http://image.tmdb.org/t/p/";
+  const imgPosterUrl = "https://image.tmdb.org/t/p/";
   const imgPosterSize = "w185/";
   let page = 1;
+  const maxPages = 5;
 
   const options = {
     method: "GET",
@@ -20,12 +21,25 @@
     x.json()
   );
 
-  function increment() {
+  function increment(event) {
+    event.preventDefault();
     page += 1;
+    if (page > maxPages) {
+      page = maxPages;
+    }
     promise = fetch(`${fetchUrl}&page=${page}`, options).then((x) => x.json());
   }
-  function decrement() {
+  function decrement(event) {
+    event.preventDefault();
     page -= 1;
+    if (page < 1) {
+      page = 1;
+    }
+    promise = fetch(`${fetchUrl}&page=${page}`, options).then((x) => x.json());
+  }
+  function navigate(event) {
+    event.preventDefault();
+    page = parseInt(event.target.textContent);
     promise = fetch(`${fetchUrl}&page=${page}`, options).then((x) => x.json());
   }
 </script>
@@ -52,17 +66,66 @@
       {/each}
     </ul>
   </section>
-  <p>{page} of {data.total_pages}</p>
-  {#if page !== 1}
+  <nav aria-label="Movie Scroll Pagination">
+    <ul class="pagination pagination-sm">
+      <li class="page-item">
+        <a
+          class="page-link bg-dark text-light"
+          aria-label="Previous"
+          href="#"
+          on:click={decrement}
+        >
+          <i class="fa fa-fw fa-caret-left"></i>
+        </a>
+      </li>
+      <li class="page-item">
+        <a class="page-link bg-dark text-light" href="#" on:click={navigate}>
+          1
+        </a>
+      </li>
+      <li class="page-item">
+        <a class="page-link bg-dark text-light" href="#" on:click={navigate}>
+          2
+        </a>
+      </li>
+      <li class="page-item">
+        <a class="page-link bg-dark text-light" href="#" on:click={navigate}>
+          3
+        </a>
+      </li>
+      <li class="page-item">
+        <a class="page-link bg-dark text-light" href="#" on:click={navigate}>
+          4
+        </a>
+      </li>
+      <li class="page-item">
+        <a class="page-link bg-dark text-light" href="#" on:click={navigate}>
+          5
+        </a>
+      </li>
+      <li class="page-item">
+        <a
+          class="page-link bg-dark text-light"
+          arai-label="Next"
+          href="#"
+          on:click={increment}
+        >
+          <i class="fa fa-fw fa-caret-right"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- <p>{page} of {maxPages}</p> -->
+  <!-- {#if page !== 1}
     <button type="button" class="btn btn-primary" on:click={decrement}>
       <i class="fa fa-fw fa-caret-left"></i>
     </button>
   {/if}
-  {#if page !== data.total_pages}
+  {#if page !== maxPages}
     <button type="button" class="btn btn-primary" on:click={increment}>
       <i class="fa fa-fw fa-caret-right"></i>
     </button>
-  {/if}
+  {/if} -->
 {:catch error}
   {error}
 {/await}
