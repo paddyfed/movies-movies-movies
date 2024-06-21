f8fb9cbacf39b0ab5d3d279ce0df70c259f733bd
Paddy <paddyfedoo@hotmail.com>
2024-06-21 12:54:36 +0100

    Added footer and attribution to themoviedb.org

---
+++ src/components/MainFooter.svelte
@@ -0,0 +1,26 @@
+<div class="container">
+  <footer
+    class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
+  >
+    <div class="d-flex align-items-center">
+      <span class="mb-3 mb-md-0">
+        © {new Date().getFullYear()} Company, Inc
+      </span>
+    </div>
+    <div class="d-flex align-items-center">
+      <span class="mb-3 mb-md-0"
+        ><span class="me-2">Information and Images from </span><a
+          href="https://www.themoviedb.org/"
+        >
+          <img
+            src="/movies-movies-movies/TmDb_Logo.svg"
+            alt="themoviedb.org"
+            title="themoviedb.org"
+            width="80"
+            height="50"
+          />
+        </a></span
+      >
+    </div>
+  </footer>
+</div>
--- src/components/MainNavBar.svelte
+++ src/components/MainNavBar.svelte
@@ -17,15 +17,15 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#top">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#top">Link</a>
         </li>
         <li class="nav-item dropdown">
           <a
             class="nav-link dropdown-toggle"
-            href="#"
+            href="#top"
             role="button"
             data-bs-toggle="dropdown"
             aria-expanded="false"
@@ -33,14 +33,18 @@
             Dropdown
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#top">Action</a></li>
+            <li><a class="dropdown-item" href="#top">Another action</a></li>
             <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li>
+              <a class="dropdown-item" href="#top">Something else here</a>
+            </li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link disabled" aria-disabled="true" href="#top">
+            Disabled
+          </a>
         </li>
       </ul>
       <form class="d-flex" role="search">
