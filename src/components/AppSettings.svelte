17d5abf3e3fe671494972d9bc05c1dba5677c25c
Paddy <paddyfedoo@hotmail.com>
2024-06-25 22:21:12 +0100

    Simple localstorage option

---
+++ src/components/AppSettings.svelte
@@ -0,0 +1,27 @@
+<script>
+  let selected = localStorage.getItem("sel")
+    ? localStorage.getItem("sel")
+    : "1";
+
+  function handleSubmit() {
+    localStorage.setItem("sel", selected);
+    console.log(selected);
+  }
+
+  function resetLocalStorage() {
+    localStorage.clear();
+    selected = "1";
+  }
+</script>
+
+<h2>Date Format</h2>
+<form on:submit|preventDefault={handleSubmit}>
+  <select name="dateSetting" id="dateSetting" bind:value={selected}>
+    <option value="1">a1</option>
+    <option value="2">a2</option>
+    <option value="3">a3</option>
+  </select>
+  <button type="submit"> Submit </button>
+</form>
+<button on:click={resetLocalStorage}>Reset to Defaults</button>
+<h2>{selected}</h2>
--- src/components/MainNavBar.svelte
+++ src/components/MainNavBar.svelte
@@ -35,10 +35,9 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#top">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#top">Link</a>
+          <a class="nav-link" href="{import.meta.env.BASE_URL}/settings"
+            >Settings</a
+          >
         </li>
         <li class="nav-item dropdown">
           <a
