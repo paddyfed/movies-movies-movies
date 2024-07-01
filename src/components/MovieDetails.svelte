2bc48aa1c63985cb7fccfe89c005a512faf19db4
Paddy <paddyfedoo@hotmail.com>
2024-07-01 16:44:16 +0100

    Using css grid to layout the movie details page

--- src/components/MovieDetails.svelte
+++ src/components/MovieDetails.svelte
@@ -18,7 +18,8 @@
   const imgPosterSize = "w342";
   const backdropSize = "w1280";
 
-  let backdrop = new URL(imgPosterUrl + backdropSize);
+  let backdropUrl = new URL(imgPosterUrl + backdropSize);
+  let posterUrl = new URL(imgPosterUrl + imgPosterSize);
 
   let promise = fetch(
     `${import.meta.env.PUBLIC_API_URL}movie/${movieIdParam}`,
@@ -30,31 +31,74 @@
   <div>Loading....</div>
 {:then data}
   <section
-    style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)), url({backdrop +
+    style="background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 1)), url({backdropUrl +
       data.backdrop_path}); background-repeat: no-repeat; background-size: cover;"
+    class="rounded p-3"
   >
-    <h1>{data.title}</h1>
-    <p>{data.overview}</p>
-    <p>
+    <h1 class="mb-3">{data.title}</h1>
+    <p class="poster">
       <img
-        src={imgPosterUrl + imgPosterSize + data.poster_path}
+        src={posterUrl + data.poster_path}
         alt={data.title + " Poster"}
         title={data.title}
       />
     </p>
-    <p>
-      Release Date: {new Date(Date.parse(data.release_date)).toLocaleDateString(
-        dateSetting,
-        {
+    <div class="details">
+      <p>{data.overview}</p>
+
+      <p>
+        Release Date: {new Date(
+          Date.parse(data.release_date)
+        ).toLocaleDateString(dateSetting, {
           year: "numeric",
           month: "long",
           day: "numeric",
-        }
-      )}
-    </p>
+        })}
+      </p>
+    </div>
 
-    <MovieTrailers movieId={movieIdParam} />
+    <div>
+      <MovieTrailers movieId={movieIdParam} />
+    </div>
   </section>
 {:catch error}
   {console.log(error)}
 {/await}
+
+<style>
+  img {
+    max-width: 100%;
+  }
+  section {
+    display: grid;
+    gap: 0.5em;
+    grid-template-columns: 1fr 50% 1fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "header header header"
+      "poster details ."
+      "footer footer footer";
+  }
+
+  h1 {
+    grid-area: header;
+  }
+
+  div {
+    grid-area: footer;
+  }
+
+  .poster {
+    grid-area: poster;
+  }
+
+  .details {
+    grid-area: details;
+  }
+
+  @media screen and (max-width: 768px) {
+    section {
+      display: block;
+    }
+  }
+</style>
