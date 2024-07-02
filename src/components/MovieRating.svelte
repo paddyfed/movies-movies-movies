cc4ac14cba6b9ace2b9548d3c957a02864485bf2
Paddy <paddyfedoo@hotmail.com>
2024-07-02 17:01:54 +0100

    Very basik rating system

--- src/components/MovieRating.svelte
+++ src/components/MovieRating.svelte
@@ -1,9 +1,36 @@
+<script>
+  let rating = localStorage.getItem("786892");
+  $: liked = rating === "liked" ? true : false;
+  $: disliked = rating === "disliked" ? true : false;
+  $: wishlist = rating === "wishlist" ? true : false;
+
+  function likeMovie(event) {
+    console.log(event.target);
+    localStorage.setItem("786892", "liked");
+    rating = localStorage.getItem("786892");
+  }
+  function dislikeMovie(event) {
+    console.log(event.target);
+    localStorage.setItem("786892", "disliked");
+    rating = localStorage.getItem("786892");
+  }
+  function wantToSeeMovie(event) {
+    console.log(event.target);
+    localStorage.setItem("786892", "wishlist");
+    rating = localStorage.getItem("786892");
+  }
+</script>
+
+{rating}
+
 <div class="form-check">
   <input
     class="form-check-input"
     type="checkbox"
     value=""
     id="flexCheckDefault"
+    bind:checked={liked}
+    on:click={likeMovie}
   />
   <label class="form-check-label" for="flexCheckDefault"> Liked </label>
 </div>
@@ -13,7 +40,8 @@
     type="checkbox"
     value=""
     id="flexCheckChecked"
-    checked
+    bind:checked={disliked}
+    on:click={dislikeMovie}
   />
   <label class="form-check-label" for="flexCheckChecked"> Disliked </label>
 </div>
@@ -23,7 +51,8 @@
     type="checkbox"
     value=""
     id="flexCheckChecked2"
-    checked
+    bind:checked={wishlist}
+    on:click={wantToSeeMovie}
   />
   <label class="form-check-label" for="flexCheckChecked2"> Want to See </label>
 </div>
