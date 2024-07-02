69bff860947546ac040bda7d5e73a146e0a9eabc
Paddy <paddyfedoo@hotmail.com>
2024-07-02 19:57:23 +0100

    Show list of liked, disliked and wished to see movies

--- src/components/ViewWishList.svelte
+++ src/components/ViewWishList.svelte
@@ -1,7 +1,35 @@
 <script>
   const localItems = { ...localStorage };
+
+  const likedList = {};
+  const dislikedList = {};
+  const wishlistList = {};
+
+  for (const key in localItems) {
+    if (Object.hasOwnProperty.call(localItems, key)) {
+      const element = localItems[key];
+      if (element === "liked") {
+        likedList[key] = localItems[key];
+      }
+      if (element === "disliked") {
+        dislikedList[key] = localItems[key];
+      }
+      if (element === "wishlist") {
+        wishlistList[key] = localItems[key];
+      }
+    }
+  }
 </script>
 
-{JSON.stringify(localItems)}
-
-<div>Hello f</div>
+<h2>Liked</h2>
+{#each Object.entries(likedList) as [key, value]}
+  <p>{key}</p>
+{/each}
+<h2>Disliked</h2>
+{#each Object.entries(dislikedList) as [key, value]}
+  <p>{key}</p>
+{/each}
+<h2>Wishlist</h2>
+{#each Object.entries(wishlistList) as [key, value]}
+  <p>{key}</p>
+{/each}
