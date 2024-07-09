e8ff7e2fb7c38a15202c537aa9b78a1196c40b92
Paddy <paddyfedoo@hotmail.com>
2024-07-09 22:09:16 +0100

    Change date format settings to horizontal form

--- src/components/AppSettings.svelte
+++ src/components/AppSettings.svelte
@@ -26,32 +26,34 @@
 
 <h2 class="mb-3">Date Format</h2>
 <form on:submit|preventDefault>
-  <div class="mb-3">
-    <label for="dateSetting" class="form-label">
+  <div class="row mb-3">
+    <label for="dateSetting" class="col-sm-2 col-form-label">
       Choose your date setting
     </label>
-    <select
-      class="form-select"
-      name="dateSetting"
-      id="dateSetting"
-      bind:value={dateSettingSelected}
-      on:change={handleDateSettingChanged}
-    >
-      <option value="en-IE"
-        >{new Date().toLocaleDateString("en-IE", longDateOptions)}</option
+    <div class="col-sm-8">
+      <select
+        class="form-select"
+        name="dateSetting"
+        id="dateSetting"
+        bind:value={dateSettingSelected}
+        on:change={handleDateSettingChanged}
       >
-      <option value="en-US"
-        >{new Date().toLocaleDateString("en-US", longDateOptions)}</option
-      >
-      <option value="de-DE"
-        >{new Date().toLocaleDateString("de-DE", longDateOptions)}</option
-      >
-      <option value="ko-KR"
-        >{new Date().toLocaleDateString("ko-KR", longDateOptions)}</option
-      >
-    </select>
-    <div class="form-text" aria-describedby="dateSetting">
-      {new Date().toLocaleDateString(dateSettingSelected, longDateOptions)}
+        <option value="en-IE"
+          >{new Date().toLocaleDateString("en-IE", longDateOptions)}</option
+        >
+        <option value="en-US"
+          >{new Date().toLocaleDateString("en-US", longDateOptions)}</option
+        >
+        <option value="de-DE"
+          >{new Date().toLocaleDateString("de-DE", longDateOptions)}</option
+        >
+        <option value="ko-KR"
+          >{new Date().toLocaleDateString("ko-KR", longDateOptions)}</option
+        >
+      </select>
+      <div class="form-text" aria-describedby="dateSetting">
+        {new Date().toLocaleDateString(dateSettingSelected, longDateOptions)}
+      </div>
     </div>
   </div>
   <div class="mb-3">
