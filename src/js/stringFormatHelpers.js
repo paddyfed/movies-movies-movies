a58c3fda22fef23d2f75a89f4ed9a9ce7444c023
Paddy <paddyfedoo@hotmail.com>
2024-08-01 16:01:47 +0100

    Adding comments to stringHelpers

--- src/js/stringFormatHelpers.js
+++ src/js/stringFormatHelpers.js
@@ -1,3 +1,5 @@
+// Purpose: Capitalizes the first letter of a string. eg welcome becomes Welcome.
+// https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
